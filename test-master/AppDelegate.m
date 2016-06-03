31f0df4c5d4d7669dc3b3e54c8a151d7fed3a50f
Pongkasem Natprasert <pongkasem@leklekstudio.com>
2016-06-04 00:19:08 +0700

    add 3rd line to 'add_third_line' branch

--- test-master/AppDelegate.m
+++ test-master/AppDelegate.m
@@ -18,6 +18,8 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     NSLog(@"application did finish launching");
+    
+    NSLog(@"application did finish launching 3");
     return YES;
 }
 
